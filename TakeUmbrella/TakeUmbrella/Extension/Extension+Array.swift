d76ef96785fb9ec5a97307d94c84dbdae3eea74a
SloWax <rjsnrl0128@naver.com>
2023-11-28 00:46:32 +0900

    update readme

--- TakeUmbrella/TakeUmbrella/Extension/Extension+Array.swift
+++ TakeUmbrella/TakeUmbrella/Extension/Extension+Array.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Array {
     func splitRange(_ index: UInt) -> Array {
-        guard self.count >= index else { return [] }
+        guard self.count > index else { return self }
         
         let result = self[0...Int(index)]
         return Array(result)
